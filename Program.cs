192464db0125bd1a88ba90d7e428f5bfea5c05e4
Guaaan <72053761+Guaaan@users.noreply.github.com>
2021-04-13 22:48:43 -0400

    pruebas segundo día"

--- Program.cs
+++ Program.cs
@@ -1,12 +1,46 @@
 ﻿using System;
-
-namespace myfirstapp
+ 
+namespace ejercicio_basicos_DDR_1
 {
-    class Program
+    class MainClass
     {
-        static void Main(string[] args)
+        public static void Main (string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            int a = 5;
+            int b = 30;
+            int c = 4;
+ 
+            int suma = a + b + c;
+ 
+            Console.WriteLine ("La suma es "+suma);
+ 
+            Console.ReadLine ();
+
+            
+            var nombre = "guan";
+            var ciudad = "Constantinopla";
+            Console.WriteLine("Hola, "+nombre+" bienvenido a "+ciudad);
+
+            var edad = 20;
+            Console.WriteLine("Hola, te llamas "+nombre+" y tienes "+edad+" años");
+
+
+            int num1 = 824352356;
+            int num2 = 134623467;
+ 
+            if (num1 >= num2) {
+                Console.WriteLine ("El primer numero es mayor o igual que el segundo");
+            } else {
+                Console.WriteLine ("El segundo numero es mayor que el primero");
+            }
+ 
+            Console.ReadLine ();
         }
+        
+        
+        
     }
 }
+// dotnet run
+// Console.WriteLine("gatooo");
